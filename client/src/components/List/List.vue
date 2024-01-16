04648c8504653aa3e94f4d475d79be3348a6a099
Julia Hubert <julia@iMac-de-Julia.lan>
2024-01-16 16:40:07 +0100

    client

--- client/src/components/List/List.vue
+++ client/src/components/List/List.vue
@@ -1,12 +1,21 @@
 <script setup>
 import { useAuthStore } from '../Features/user/store/authStore';
+import { useUsersStore } from '../Features/user/store/usersStore';
 import CollaborateurCard from '../Home/CollaborateurCard.vue';
+
+const usersStore = useUsersStore()
+
 </script>
 
 <template>
-    <CollaborateurCard v-for="user in users" :key="user.id" :item="user" />
+    <div class="flex flex-row flex-wrap justify-center la-class-de-julia">
+        
+        <CollaborateurCard v-for="user in usersStore.users" :key="user._id" :item="user" />
+    </div>
 </template>
 
 <style scoped>
-
+.la-class-de-julia{
+    
+}
 </style>
