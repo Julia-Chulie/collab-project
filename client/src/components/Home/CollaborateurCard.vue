8be3f6a2e855cb220d65354876173f012e2beb6f
Thibaud BASSET <basset.thibaud@gmail.com>
2024-01-16 16:58:40 +0100

    Update CollaborateurCard.vue

--- client/src/components/Home/CollaborateurCard.vue
+++ client/src/components/Home/CollaborateurCard.vue
@@ -39,10 +39,11 @@ const deleteUser = (id) => {
 </script>
 
 <template>
-<div class="flex shadow shadow-gray-500 m-2 card" >
+<div class="flex shadow shadow-gray-500 m-2 card relative" >
   <div :style="{ backgroundImage: 'url(' + item.photo + ')' }" class="bg-cover bg-no-repeat w-[200px] bg-center" ></div>
-  <div class="flex flex-col justify-center items-start gap-2 p-10 relative">
-    <div class='absolute right-0 top-0 p-1 text-white font-bold' :class="{'bg-blue-400': (item.category === 'Technique'), 'bg-rose-300': (item.category === 'Marketing'), 'bg-green-400': (item.category === 'Client')} ">{{ item.category }}</div>
+  <div class='absolute right-0 top-0 p-1 text-white font-bold' :class="{'bg-blue-400': (item.category === 'Technique'), 'bg-rose-300': (item.category === 'Marketing'), 'bg-green-400': (item.category === 'Client')} ">{{ item.category }}
+  </div>
+  <div class="flex flex-col justify-center items-start gap-2 p-10">
     <div>
       <strong>{{ item.firstname }} {{ item.lastname }}</strong>
       <span class="text-gray-400 pl-1">({{ moment().diff(item.birthdate, 'years') }} ans)</span>
@@ -73,6 +74,5 @@ const deleteUser = (id) => {
   .card{
     height: 14rem;
     width: 34rem;
-    justify-content: space-between;
   }
 </style>
